5ffd04b2a8f4bbe3a708554bfc3a3b9945fc60e2
Gogs <gogs@fake.local>
2018-12-17 15:33:33 +0500

    adding dist

--- changes.js
+++ changes.js
@@ -1,10 +1,10 @@
-    // remove all 'д.е.', 'де'
-    expr = expr.replace(/д\.е\./g, "");
-    expr = expr.replace(/де/g, "");
+    // remove all 'Рґ.Рµ.', 'РґРµ'
+    expr = expr.replace(/Рґ\.Рµ\./g, "");
+    expr = expr.replace(/РґРµ/g, "");
 
 
   parse.isValidLatinOrGreek = function isValidLatinOrGreek(c) {
-    return (/^[a-zA-Zа-яА-Я_$\u00C0-\u02AF\u0370-\u03FF\u2100-\u214F]$/.test(c)
+    return (/^[a-zA-ZР°-СЏРђ-РЇ_$\u00C0-\u02AF\u0370-\u03FF\u2100-\u214F]$/.test(c)
     );
   };
 
@@ -13,7 +13,7 @@
   function parseUnit() {
     var unitName = '';
 
-    // Alphanumeric characters only; matches [a-zA-Z0-9а-яА-Я.%]
+    // Alphanumeric characters only; matches [a-zA-Z0-9Р°-СЏРђ-РЇ.%]
     // . = 46
     // % = 37
     var code = text.charCodeAt(index);
@@ -72,27 +72,27 @@
 
 
       //rus
-      'да': { name: 'да', value: 1e1, scientific: false },
-      'г': { name: 'г', value: 1e2, scientific: false },
-      'к': { name: 'к', value: 1e3, scientific: true },
-      'М': { name: 'М', value: 1e6, scientific: true },
-      'Г': { name: 'Г', value: 1e9, scientific: true },
-      'Т': { name: 'Т', value: 1e12, scientific: true },
-      'П': { name: 'П', value: 1e15, scientific: true },
-      'Э': { name: 'Э', value: 1e18, scientific: true },
-      'З': { name: 'З', value: 1e21, scientific: true },
-      'И': { name: 'И', value: 1e24, scientific: true },
+      'РґР°': { name: 'РґР°', value: 1e1, scientific: false },
+      'Рі': { name: 'Рі', value: 1e2, scientific: false },
+      'Рє': { name: 'Рє', value: 1e3, scientific: true },
+      'Рњ': { name: 'Рњ', value: 1e6, scientific: true },
+      'Р“': { name: 'Р“', value: 1e9, scientific: true },
+      'Рў': { name: 'Рў', value: 1e12, scientific: true },
+      'Рџ': { name: 'Рџ', value: 1e15, scientific: true },
+      'Р­': { name: 'Р­', value: 1e18, scientific: true },
+      'Р—': { name: 'Р—', value: 1e21, scientific: true },
+      'Р': { name: 'Р', value: 1e24, scientific: true },
 
-      'д': { name: 'д', value: 1e-1, scientific: false },
-      'с': { name: 'с', value: 1e-2, scientific: false },
-      'м': { name: 'м', value: 1e-3, scientific: true },
-      'мк': { name: 'мк', value: 1e-6, scientific: true },
-      'н': { name: 'н', value: 1e-9, scientific: true },
-      'п': { name: 'п', value: 1e-12, scientific: true },
-      'ф': { name: 'ф', value: 1e-15, scientific: true },
-      'а': { name: 'а', value: 1e-18, scientific: true },
-      'з': { name: 'з', value: 1e-21, scientific: true },
-      'и': { name: 'и', value: 1e-24, scientific: true }
+      'Рґ': { name: 'Рґ', value: 1e-1, scientific: false },
+      'СЃ': { name: 'СЃ', value: 1e-2, scientific: false },
+      'Рј': { name: 'Рј', value: 1e-3, scientific: true },
+      'РјРє': { name: 'РјРє', value: 1e-6, scientific: true },
+      'РЅ': { name: 'РЅ', value: 1e-9, scientific: true },
+      'Рї': { name: 'Рї', value: 1e-12, scientific: true },
+      'С„': { name: 'С„', value: 1e-15, scientific: true },
+      'Р°': { name: 'Р°', value: 1e-18, scientific: true },
+      'Р·': { name: 'Р·', value: 1e-21, scientific: true },
+      'Рё': { name: 'Рё', value: 1e-24, scientific: true }
 
 
 
@@ -110,87 +110,87 @@
 
 
       //rus
-      'дека': { name: 'дека', value: 1e1, scientific: false },
-      'гекто': { name: 'гекто', value: 1e2, scientific: false },
-      'кило': { name: 'кило', value: 1e3, scientific: true },
-      'мега': { name: 'мега', value: 1e6, scientific: true },
-      'гига': { name: 'гига', value: 1e9, scientific: true },
-      'тера': { name: 'тера', value: 1e12, scientific: true },
-      'пета': { name: 'пета', value: 1e15, scientific: true },
-      'экса': { name: 'экса', value: 1e18, scientific: true },
-      'зетта': { name: 'зетта', value: 1e21, scientific: true },
-      'иотта': { name: 'иотта', value: 1e24, scientific: true },
+      'РґРµРєР°': { name: 'РґРµРєР°', value: 1e1, scientific: false },
+      'РіРµРєС‚Рѕ': { name: 'РіРµРєС‚Рѕ', value: 1e2, scientific: false },
+      'РєРёР»Рѕ': { name: 'РєРёР»Рѕ', value: 1e3, scientific: true },
+      'РјРµРіР°': { name: 'РјРµРіР°', value: 1e6, scientific: true },
+      'РіРёРіР°': { name: 'РіРёРіР°', value: 1e9, scientific: true },
+      'С‚РµСЂР°': { name: 'С‚РµСЂР°', value: 1e12, scientific: true },
+      'РїРµС‚Р°': { name: 'РїРµС‚Р°', value: 1e15, scientific: true },
+      'СЌРєСЃР°': { name: 'СЌРєСЃР°', value: 1e18, scientific: true },
+      'Р·РµС‚С‚Р°': { name: 'Р·РµС‚С‚Р°', value: 1e21, scientific: true },
+      'РёРѕС‚С‚Р°': { name: 'РёРѕС‚С‚Р°', value: 1e24, scientific: true },
 
-      'деци': { name: 'деци', value: 1e-1, scientific: false },
-      'санти': { name: 'санти', value: 1e-2, scientific: false },
-      'мили': { name: 'мили', value: 1e-3, scientific: true },
-      'микро': { name: 'микро', value: 1e-6, scientific: true },
-      'нано': { name: 'нано', value: 1e-9, scientific: true },
-      'пико': { name: 'пико', value: 1e-12, scientific: true },
-      'фемто': { name: 'фемто', value: 1e-15, scientific: true },
-      'атто': { name: 'атто', value: 1e-18, scientific: true },
-      'зепто': { name: 'зепто', value: 1e-21, scientific: true },
-      'иокто': { name: 'иокто', value: 1e-24, scientific: true }
+      'РґРµС†Рё': { name: 'РґРµС†Рё', value: 1e-1, scientific: false },
+      'СЃР°РЅС‚Рё': { name: 'СЃР°РЅС‚Рё', value: 1e-2, scientific: false },
+      'РјРёР»Рё': { name: 'РјРёР»Рё', value: 1e-3, scientific: true },
+      'РјРёРєСЂРѕ': { name: 'РјРёРєСЂРѕ', value: 1e-6, scientific: true },
+      'РЅР°РЅРѕ': { name: 'РЅР°РЅРѕ', value: 1e-9, scientific: true },
+      'РїРёРєРѕ': { name: 'РїРёРєРѕ', value: 1e-12, scientific: true },
+      'С„РµРјС‚Рѕ': { name: 'С„РµРјС‚Рѕ', value: 1e-15, scientific: true },
+      'Р°С‚С‚Рѕ': { name: 'Р°С‚С‚Рѕ', value: 1e-18, scientific: true },
+      'Р·РµРїС‚Рѕ': { name: 'Р·РµРїС‚Рѕ', value: 1e-21, scientific: true },
+      'РёРѕРєС‚Рѕ': { name: 'РёРѕРєС‚Рѕ', value: 1e-24, scientific: true }
 
 
 
 
       //rus
       ,
-      'да': { name: 'да', value: 1e2, scientific: false },
-      'г': { name: 'г', value: 1e4, scientific: false },
-      'к': { name: 'к', value: 1e6, scientific: true },
-      'М': { name: 'М', value: 1e12, scientific: true },
-      'Г': { name: 'Г', value: 1e18, scientific: true },
-      'Т': { name: 'Т', value: 1e24, scientific: true },
-      'П': { name: 'П', value: 1e30, scientific: true },
-      'Э': { name: 'Э', value: 1e36, scientific: true },
-      'З': { name: 'З', value: 1e42, scientific: true },
-      'И': { name: 'И', value: 1e48, scientific: true },
+      'РґР°': { name: 'РґР°', value: 1e2, scientific: false },
+      'Рі': { name: 'Рі', value: 1e4, scientific: false },
+      'Рє': { name: 'Рє', value: 1e6, scientific: true },
+      'Рњ': { name: 'Рњ', value: 1e12, scientific: true },
+      'Р“': { name: 'Р“', value: 1e18, scientific: true },
+      'Рў': { name: 'Рў', value: 1e24, scientific: true },
+      'Рџ': { name: 'Рџ', value: 1e30, scientific: true },
+      'Р­': { name: 'Р­', value: 1e36, scientific: true },
+      'Р—': { name: 'Р—', value: 1e42, scientific: true },
+      'Р': { name: 'Р', value: 1e48, scientific: true },
 
-      'д': { name: 'д', value: 1e-2, scientific: false },
-      'с': { name: 'с', value: 1e-4, scientific: false },
-      'м': { name: 'м', value: 1e-6, scientific: true },
-      'мк': { name: 'мк', value: 1e-12, scientific: true },
-      'н': { name: 'н', value: 1e-18, scientific: true },
-      'п': { name: 'п', value: 1e-24, scientific: true },
-      'ф': { name: 'ф', value: 1e-30, scientific: true },
-      'а': { name: 'а', value: 1e-36, scientific: true },
-      'з': { name: 'з', value: 1e-42, scientific: true },
-      'и': { name: 'и', value: 1e-48, scientific: true }
+      'Рґ': { name: 'Рґ', value: 1e-2, scientific: false },
+      'СЃ': { name: 'СЃ', value: 1e-4, scientific: false },
+      'Рј': { name: 'Рј', value: 1e-6, scientific: true },
+      'РјРє': { name: 'РјРє', value: 1e-12, scientific: true },
+      'РЅ': { name: 'РЅ', value: 1e-18, scientific: true },
+      'Рї': { name: 'Рї', value: 1e-24, scientific: true },
+      'С„': { name: 'С„', value: 1e-30, scientific: true },
+      'Р°': { name: 'Р°', value: 1e-36, scientific: true },
+      'Р·': { name: 'Р·', value: 1e-42, scientific: true },
+      'Рё': { name: 'Рё', value: 1e-48, scientific: true }
 
 
 
 
       //rus
-      'к': { name: 'к', value: 1e3, scientific: true },
-      'М': { name: 'М', value: 1e6, scientific: true },
-      'Г': { name: 'Г', value: 1e9, scientific: true },
-      'Т': { name: 'Т', value: 1e12, scientific: true },
-      'П': { name: 'П', value: 1e15, scientific: true },
-      'Э': { name: 'Э', value: 1e18, scientific: true },
-      'З': { name: 'З', value: 1e21, scientific: true },
-      'И': { name: 'И', value: 1e24, scientific: true },
+      'Рє': { name: 'Рє', value: 1e3, scientific: true },
+      'Рњ': { name: 'Рњ', value: 1e6, scientific: true },
+      'Р“': { name: 'Р“', value: 1e9, scientific: true },
+      'Рў': { name: 'Рў', value: 1e12, scientific: true },
+      'Рџ': { name: 'Рџ', value: 1e15, scientific: true },
+      'Р­': { name: 'Р­', value: 1e18, scientific: true },
+      'Р—': { name: 'Р—', value: 1e21, scientific: true },
+      'Р': { name: 'Р', value: 1e24, scientific: true },
 
 
 
 
 
       // rus
-      'кило': { name: 'кило', value: 1e3, scientific: true },
-      'мега': { name: 'мега', value: 1e6, scientific: true },
-      'гига': { name: 'гига', value: 1e9, scientific: true },
-      'тера': { name: 'тера', value: 1e12, scientific: true },
-      'пета': { name: 'пета', value: 1e15, scientific: true },
-      'экса': { name: 'экса', value: 1e18, scientific: true },
-      'зетта': { name: 'зетта', value: 1e21, scientific: true },
-      'иотта': { name: 'иотта', value: 1e24, scientific: true },
+      'РєРёР»Рѕ': { name: 'РєРёР»Рѕ', value: 1e3, scientific: true },
+      'РјРµРіР°': { name: 'РјРµРіР°', value: 1e6, scientific: true },
+      'РіРёРіР°': { name: 'РіРёРіР°', value: 1e9, scientific: true },
+      'С‚РµСЂР°': { name: 'С‚РµСЂР°', value: 1e12, scientific: true },
+      'РїРµС‚Р°': { name: 'РїРµС‚Р°', value: 1e15, scientific: true },
+      'СЌРєСЃР°': { name: 'СЌРєСЃР°', value: 1e18, scientific: true },
+      'Р·РµС‚С‚Р°': { name: 'Р·РµС‚С‚Р°', value: 1e21, scientific: true },
+      'РёРѕС‚С‚Р°': { name: 'РёРѕС‚С‚Р°', value: 1e24, scientific: true },
 
 
 
 
 
 var isValidAlpha = function isValidAlpha(p) {
-        return (/^[a-zA-Zа-яА-Я.%]$/.test(p)
+        return (/^[a-zA-ZР°-СЏРђ-РЇ.%]$/.test(p)
         );
       };
